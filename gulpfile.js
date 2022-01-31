d32b0d62ba30f344f3c3290b6b036a7f1e7f89e3
Ihor <72511454+padlukad@users.noreply.github.com>
2022-01-31 16:12:25 +0200

    video fashion

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,8 @@ function styles() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
-        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/@fancyapps/ui/dist/fancybox.umd.js',
+        'node_modules/slick-carousel/slick/slick.js',   
         'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
